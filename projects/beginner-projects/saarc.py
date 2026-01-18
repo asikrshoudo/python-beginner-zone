94074767f4ece079a8807e698f935d93b633662d
asikrshoudo <shoudoashikurshek@gmail.com>
2026-01-18 23:47:07 +0600

    added a new python file named by saarc.py

---
+++ projects/beginner-projects/saarc.py
@@ -0,0 +1,10 @@
+saarc = ["bangladesh","afghanistan","bhutan","nepal","india","pakistan","sri lanka"]
+
+country = input("enter the name of the country: ")
+if country in saarc:
+    print(country, "is a member of SAARC")
+else:
+    print(country, "is not a member of SAARC")
+
+print("program terminated")
+
